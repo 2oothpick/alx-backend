b1d3465173e854514f79e9d497debe457849fda0
2oothpick <kobbyboateng14@gmail.com>
2024-04-03 17:08:45 +0000

    Add documentation

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -16,13 +16,17 @@ class BasicCache(BaseCaching):
     def put(self, key, item):
         """
         Populates the self.cache_data dictionary
+        if key and item anre not None otherwise
+        it does nothing.
         """
-        if key is not None or item is not None:
+        if key is not None and item is not None:
             self.cache_data.update({key: item})
 
     def get(self, key):
         """
         Returns dictionary values for the provided key
+        if key is exists in self.cache_data and the key is
+        not None otherwise its returns None
         """
         if key in self.cache_data.keys() and key is not None:
             return self.cache_data.get(key)
